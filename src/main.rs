0f3e584faab0b7d6747b8cee59009dfab41e16d5
Jan Lafferton <jan@lafferton.de>
2022-07-29 11:01:57 -0700

    key handling + buffer swapping

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 extern crate glfw;
 
-use glfw::Context;
+use glfw::{Action, Context, Key};
 
 fn main() {
     let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
-    let (mut window, _) = glfw.create_window(
+    let (mut window, events) = glfw.create_window(
         800,
         600,
         "ray-tracer",
@@ -13,8 +13,21 @@ fn main() {
     ).expect("Failed to create window");
 
     window.make_current();
+    window.set_key_polling(true);
 
     while !window.should_close() {
+        window.swap_buffers();
+
         glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events){
+            println!("{:?}", event);
+
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true);
+                }
+                _ => {}
+            }
+        }
     }
 }
