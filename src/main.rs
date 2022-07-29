b2178aad60fcce2da64dfaa70ea9db770fb3eddf
Jan Lafferton <jan@lafferton.de>
2022-07-29 10:58:13 -0700

    open simple glfw window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+extern crate glfw;
+
+use glfw::Context;
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    let (mut window, _) = glfw.create_window(
+        800,
+        600,
+        "ray-tracer",
+        glfw::WindowMode::Windowed
+    ).expect("Failed to create window");
+
+    window.make_current();
+
+    while !window.should_close() {
+        glfw.poll_events();
+    }
 }
