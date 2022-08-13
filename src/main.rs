fbabbfe52614fafa1baf3080282d73f3d6de2239
Jan Lafferton <jan@lafferton.de>
2022-08-13 23:13:01 +0200

    wgpu pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
+mod state;
+
+use wgpu::SurfaceError;
+use state::State;
+
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder
 };
 
-fn main() {
+async fn run() {
     env_logger::init();
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
+    let mut state = State::new(&window).await;
+
     event_loop.run(move |event, _, control_flow| match event {
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+                Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+                Err(e) => eprintln!("{:?}", e),
+            }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw()
+        }
         Event::WindowEvent { ref event, window_id } => {
-            if window_id == window.id() {
+            if window_id == window.id() && !state.input(event) {
                 match event {
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
+                    }
                     WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
                     _ => {}
                 }
@@ -21,4 +46,8 @@ fn main() {
         },
         _ => {}
     });
+}
+
+fn main() {
+    pollster::block_on(run());
 }
